3740dd193176b3bb46efdceb875b3e98c60451b8
krazyowl <mrkrazyowl@gmail.com>
2018-04-01 02:56:08 -0700

    Updated Realm

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let data = Data()
+        data.name = "Krazyowl"
+        data.age = 30
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
         
         return true
     }
